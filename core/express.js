a65d0366b026503054e8f15bdcabcf91c1020b58
Dev-Siri <s0431668@gmail.com>
2022-09-12 13:46:01 +0530

    change folder structure

---
+++ core/express.js
@@ -0,0 +1,8 @@
+"use strict";
+
+import express from "express";
+
+const Express = express();
+const Router = express.Router();
+
+export { Express, Router };
---
+++ src/Application.js
@@ -0,0 +1,37 @@
+"use strict";
+
+import { Express, Router } from "../core/express.js";
+
+module.exports = class Application {
+  Listen(PORT, Callback) {
+    Express.listen(PORT || 8080);
+
+    if (Callback) Callback();
+  }
+
+  Route(Type, Route, Callback) {
+    switch (Type.toUpperCase()) {
+      case "GET":
+        Express.get(Route, Callback);
+      case "POST":
+        Express.post(Route, Callback);
+      case "PATCH":
+        Express.patch(Route, Callback);
+      case "PUT":
+        Express.put(Route, Callback);
+      case "DELETE":
+        Express.delete(Route, Callback);
+    }
+  }
+
+  Use(Option1, Option2) {
+    if (!Option1)
+      throw new ReferenceError("undefined is not a plugin or option");
+
+    Express.use(Option2);
+  }
+
+  get Router() {
+    return Router;
+  }
+};
